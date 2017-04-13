bc04e370854512c1214a99605325b554cfc06f96
Alan <a-ashbeck@northwestern.edu>
2017-04-12 19:23:45 -0500

    Update content

--- server.js
+++ server.js
@@ -13,10 +13,5 @@ app.listen(PORT, function() {
   console.log('App running on port ' + PORT);
 });
 
-// app.get('/', function(req, res) {
-//     // Ship it up!
-//     res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
-
 // Require the routes set in burger_controller.js
 require('./controllers/profiles_controller.js')(app);
